1ef3f41ac36c1f61f4f10c9dc12e3e3cc49401eb
nkunic <45877400+nkunic@users.noreply.github.com>
2023-04-14 11:55:49 +0200

    5 files modified

--- src/app/color-picker/color-picker.component.ts
+++ src/app/color-picker/color-picker.component.ts
@@ -1,16 +1,10 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-color-picker',
   templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.scss']
+  styleUrls: ['./color-picker.component.scss'],
 })
 export class ColorPickerComponent {
-
-  @Input() id: string | undefined;
-  @Input() value: string | undefined;
-  @Input() title: string | undefined;
-
   @Output() colorSelected = new EventEmitter<string>();
-
 }
